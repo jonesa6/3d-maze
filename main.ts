a1bcdfb756bd173324f1922c592f65707d54846b
jonesa6 <156232904+jonesa6@users.noreply.github.com>
2024-04-18 14:36:15 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,6 +9,12 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.ultimate_star_rainbow, function 
     game.setGameOverMessage(true, "Congrats you finished in" + sec + "secs")
     game.gameOver(true)
 })
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    Render.setAttribute(Render.attribute.fov, 0.5)
+    timer.after(50, function () {
+        Render.setAttribute(Render.attribute.fov, 0.1)
+    })
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     Render.move(mySprite, 100, -100)
 })
@@ -57,6 +63,9 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.star2, function (sprite, otherSp
         . . . . . . . . . . . . . . . . 
         `, SpriteKind.star3)
     tiles.placeOnTile(mySprite5, tiles.getTileLocation(49, 49))
+})
+controller.B.onEvent(ControllerButtonEvent.Released, function () {
+    Render.setAttribute(Render.attribute.fov, 1)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.star, function (sprite, otherSprite) {
     sprites.destroy(mySprite2)
@@ -111,7 +120,7 @@ let mySprite6: Sprite = null
 let sec = 0
 let mySprite2: Sprite = null
 let mySprite: Sprite = null
-game.showLongText("Welcome to 3D Mazes. Use WASD to move or arrows, press A to jump.", DialogLayout.Full)
+game.showLongText("Welcome to 3D Mazes. Use WASD to move or arrows, press A to jump. B to zoom", DialogLayout.Full)
 mySprite = sprites.create(img`
     . . . . . . f f f f . . . . . . 
     . . . . f f f 2 2 f f f . . . . 
@@ -133,7 +142,7 @@ mySprite = sprites.create(img`
 mySprite = Render.getRenderSpriteVariable()
 tiles.setCurrentTilemap(tilemap`level2`)
 Render.moveWithController(3, 2, 1)
-Render.setAttribute(Render.attribute.fov, 0.66)
+Render.setAttribute(Render.attribute.fov, 1)
 Render.setAttribute(Render.attribute.dirX, 0.066)
 Render.setAttribute(Render.attribute.dirY, 0.066)
 Render.setAttribute(Render.attribute.wallZScale, 1.6)
@@ -164,5 +173,83 @@ game.onUpdate(function () {
     if (seconds_calculating == 1000) {
         sec += 1
         seconds_calculating = 0
+    }
+})
+game.onUpdateInterval(500, function () {
+    if (characterAnimations.matchesRule(mySprite, characterAnimations.rule(Predicate.Moving))) {
+        animation.runImageAnimation(
+        mySprite,
+        [img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 2 2 2 2 2 2 e e f . . 
+            . . f e 2 f f f f f f 2 e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f 4 4 f b f e f f . 
+            . f e e 4 1 f d d f 1 4 e e f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e 4 4 4 4 e e f . . . 
+            . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 2 2 2 2 2 2 e e f . . 
+            . f f e 2 f f f f f f 2 e f f . 
+            . f f f f f e e e e f f f f f . 
+            . . f e f b f 4 4 f b f e f . . 
+            . . f e 4 1 f d d f 1 4 e f . . 
+            . . . f e 4 d d d d 4 e f e . . 
+            . . f e f 2 2 2 2 e d d 4 e . . 
+            . . e 4 f 2 2 2 2 e d d e . . . 
+            . . . . f 4 4 5 5 f e e . . . . 
+            . . . . f f f f f f f . . . . . 
+            . . . . f f f . . . . . . . . . 
+            `,img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 2 2 2 2 2 2 e e f . . 
+            . . f e 2 f f f f f f 2 e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f 4 4 f b f e f f . 
+            . f e e 4 1 f d d f 1 4 e e f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e 4 4 4 4 e e f . . . 
+            . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f e e 2 2 2 2 2 2 e f f . . 
+            . f f e 2 f f f f f f 2 e f f . 
+            . f f f f f e e e e f f f f f . 
+            . . f e f b f 4 4 f b f e f . . 
+            . . f e 4 1 f d d f 1 4 e f . . 
+            . . e f e 4 d d d d 4 e f . . . 
+            . . e 4 d d e 2 2 2 2 f e f . . 
+            . . . e d d e 2 2 2 2 f 4 e . . 
+            . . . . e e f 5 5 4 4 f . . . . 
+            . . . . . f f f f f f f . . . . 
+            . . . . . . . . . f f f . . . . 
+            `],
+        500,
+        false
+        )
     }
 })
